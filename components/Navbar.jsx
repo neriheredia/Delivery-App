f98a67109327b91231728b9fda93e0f92fc120ca
neriheredia <developer.heredia@gmail.com>
2022-02-28 16:09:44 -0300

    link

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import styles from '../styles/Navbar.module.css'
-import Link from 'next/Link'
+import Link from 'next/link'
 import { useSelector } from 'react-redux'
 
 const Navbar = () => {
